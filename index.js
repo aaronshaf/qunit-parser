20170ae84d1aa335c317e92972c96f74465b2e51
Aaron Shafovaloff <ashafovaloff@instructure.com>
2014-01-29 10:17:30 -0700

    WIP

---
+++ index.js
@@ -0,0 +1,40 @@
+function parseLog(value) {
+  var data = {};
+  
+  var lines = value.match(/[^\r\n]+/g);
+  
+  var traceLines = extractTrace(lines);
+  if(traceLines) data.stack = traceLines;
+  
+  var actual = extractSection('Actual: ',lines);
+  if(actual.length) data.actual = actual;
+  
+  var expected = extractSection('Expected: ',lines);
+  if(expected.length) data.expected = expected;
+  
+  if(lines.length) data.description = lines;
+  
+  return data;
+}
+
+function extractTrace(lines) {
+  var traceLines = [];
+  while(lines.length && lines[lines.length - 1].trim().indexOf('at ') === 0) {
+     traceLines.push(lines.pop());
+  }
+  return traceLines;
+}
+
+function extractSection(text,lines) {
+  for(var x = 0;x < lines.length;x++) {
+    if(lines[x].indexOf(text) === 0) {
+      lines[x] = lines[x].substr(text.length);
+      return lines.splice(x);
+    }
+  }
+  return [];  
+}
+
+module.exports = {
+  parseLog: parseLog
+};
